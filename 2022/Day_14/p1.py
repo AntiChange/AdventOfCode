4d23e0afe4a55ce4e1fc83c2b9ab07502066d450
AntiChange <chr1sf4ng@gmail.com>
2022-12-14 01:31:01 -0500

    Day 14

---
+++ 2022/Day_14/p1.py
@@ -0,0 +1,61 @@
+def parse(filename):
+    inputlines = []
+    
+    with open(filename, 'r') as f:
+        for i in f.readlines():
+            inputlines.append(i.strip())
+    
+    return inputlines
+
+def solution(input):
+    # iterate once to find maxes necessary to create board
+    maxRow = 0
+    maxCol = 0
+    for line in input:
+        pairList = [x.strip().split(",") for x in line.split("->")]
+        for pair in pairList:
+            maxRow = max(maxRow, int(pair[1]))
+            maxCol = max(maxCol, int(pair[0]))
+
+    # Add 1 to maxRow/maxCol for 1-indexed
+    board = [(["."] * (maxCol + 1)) for i in range(maxRow + 1)]
+    
+    # Generate paths in board
+    # Note pairs are in order [col, row]
+    for line in input:
+        pairList = [x.strip().split(",") for x in line.split("->")]
+        prevPair = pairList[0]
+        for i in range(1, len(pairList)):
+            if pairList[i][0] == prevPair[0]:
+                for j in range(min(int(pairList[i][1]), int(prevPair[1])), max(int(pairList[i][1]), int(prevPair[1])) + 1):
+                    board[j][int(prevPair[0])] = "#"
+            else:
+                for j in range(min(int(pairList[i][0]), int(prevPair[0])), max(int(pairList[i][0]), int(prevPair[0])) + 1):
+                    board[int(prevPair[1])][j] = "#"
+            prevPair = pairList[i]
+    
+    restCount = 0
+    sandPos = [0, 500]
+
+    # Exit condition 1 - sand flows into the abyss
+    while (sandPos[0] != maxRow):
+        if (board[sandPos[0] + 1][sandPos[1]] == "."):
+            sandPos[0] += 1
+        elif (board[sandPos[0] + 1][sandPos[1] - 1] == "."):
+            sandPos[0] += 1
+            sandPos[1] -= 1
+        elif (board[sandPos[0] + 1][sandPos[1] + 1] == "."):
+            sandPos[0] += 1
+            sandPos[1] += 1
+        else:
+            restCount += 1
+            # Exit condition 2 - sand flow gets blocked. (Unlikely to happen)
+            if (sandPos == [0, 500]):
+                break
+            board[sandPos[0]][sandPos[1]] = "o"
+            sandPos = [0, 500]
+
+    return restCount
+
+
+print(solution(parse("input.txt")))
